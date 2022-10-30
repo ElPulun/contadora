65db0c978400b3023e25d71f4edd26dc5d93033d
ElPulun <116989160+ElPulun@users.noreply.github.com>
2022-10-29 22:05:44 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+    <title>Agencia De Contadores - Home</title>
+
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+
+</head>
+
+<body>
+
+    <!-- Navegation Menu -->
+
+    <div class="navbar-fixed">
+
+        <nav class="black">
+
+            <div class="nav-wrapper">
+
+                <a href="#" class="brand-logo"><i class="material-icons">code</i>Paola</a>
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="#">Noticiasssss</a></li>
+                    <li><a href="#">Noticias Paula</a></li>
+                    <li><a href="#">Mas De Paula</a></li>
+                </ul>
+            </div>
+
+        </nav>
+
+
+    </div>
+
+
+    <!-- Slider -->
+    <section class="slider">
+        <ul class="slides">
+            <li>
+                <img src="img/agency-1.jpg"> <!-- random image -->
+                <div class="caption center-align">
+                    <h1>Agencia De Contadores</h3>
+                        <h5 class="light grey-text text-lighten-3">Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Facilis suscipit odio, explicabo sapiente totam nesciunt.</h5>
+                        <a href="#" class="btn btn-large white black-text waves-effect waves-grey">Saber Mas</a>
+                </div>
+            </li>
+            <li>
+                <img src="img/agency-2.jpg"> <!-- random image -->
+                <div class="caption left-align">
+                    <h3>Conocenos</h3>
+                    <h5 class="light grey-text text-lighten-3">Servicios</h5>
+                    <a href="#" class="btn btn-large white black-text waves-effect waves-grey">Saber Mas</a>
+                </div>
+            </li>
+            <li>
+                <img src="img/agency-3.jpg"> <!-- random image -->
+                <div class="caption right-align">
+                    <h3>LLamanos</h3>
+                    <h5 class="light grey-text text-lighten-3">Mas Servicios</h5>
+                    <a href="#" class="btn btn-large white black-text waves-effect waves-grey">Saber Mas</a>
+                </div>
+            </li>
+
+        </ul>
+    </section>
+
+    <main>
+
+        <div class="container">
+
+            <div class="section">
+
+                <!--   Icon Section   -->
+                <div class="row">
+
+                    <div class="col s12 m4 l4">
+                        <div class="icon-block">
+                            <h2 class="center black-text"><i class="material-icons">flash_on</i></h2>
+                            <h5 class="center">Speeds up development</h5>
+
+                            <p class="light">We did most of the heavy lifting for you to provide a default stylings that
+                                incorporate our custom components. Additionally, we refined animations and transitions
+                                to provide a smoother experience for developers.</p>
+                        </div>
+                    </div>
+
+                    <div class="col s12 m4 l4">
+                        <div class="icon-block">
+                            <h2 class="center black-text"><i class="material-icons">group</i></h2>
+                            <h5 class="center">User Experience Focused</h5>
+
+                            <p class="light">By utilizing elements and principles of Material Design, we were able to
+                                create a framework that incorporates components and animations that provide more
+                                feedback to users. Additionally, a single underlying responsive system across all
+                                platforms allow for a more unified user experience.</p>
+                        </div>
+                    </div>
+
+                    <div class="col s12 m4 l4">
+                        <div class="icon-block">
+                            <h2 class="center black-text"><i class="material-icons">settings</i></h2>
+                            <h5 class="center">Easy to work with</h5>
+
+                            <p class="light">We have provided detailed documentation as well as specific code examples
+                                to help new users get started. We are also always open to feedback and can answer any
+                                questions a user may have about Materialize.</p>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+            <br><br>
+        </div>
+
+        <!-- Blog -->
+        <section class="container">
+
+            <h2 class="center">AGENCIA DE CONTADORES</h2>
+
+            <div class="row">
+
+                <div class="col s12 m6 l4">
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator"
+                                src="https://crehana-blog.s3.amazonaws.com/media/filer_public/af/f3/aff302fb-8cbc-4dfc-b648-bc467ed3fb14/como-iniciarte-en-diseno-lunes.jpg">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">Pautas
+                                <i class="material-icons right">more_vert</i></span>
+                            <div class="card-action center">
+
+                                <button class="btn black waves-effect waves-teal">Saber MAs</button>
+
+                            </div>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Pautas<i
+                                    class="material-icons right">close</i></span>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum at tenetur mollitia
+                                voluptate? Tempore, itaque.</p>
+                            <img src="https://crehana-blog.s3.amazonaws.com/media/filer_public/af/f3/aff302fb-8cbc-4dfc-b648-bc467ed3fb14/como-iniciarte-en-diseno-lunes.jpg"
+                                class="responsive-img">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col s12 m6 l4">
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator"
+                                src="https://crehana-blog.s3.amazonaws.com/media/filer_public/af/f3/aff302fb-8cbc-4dfc-b648-bc467ed3fb14/como-iniciarte-en-diseno-lunes.jpg">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">Pautas
+                                <i class="material-icons right">more_vert</i></span>
+                            <div class="card-action center">
+
+                                <button class="btn black waves-effect waves-teal">Saber MAs</button>
+
+                            </div>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Pautas<i
+                                    class="material-icons right">close</i></span>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum at tenetur mollitia
+                                voluptate? Tempore, itaque.</p>
+                            <img src="https://crehana-blog.s3.amazonaws.com/media/filer_public/af/f3/aff302fb-8cbc-4dfc-b648-bc467ed3fb14/como-iniciarte-en-diseno-lunes.jpg"
+                                class="responsive-img">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col s12 m6 l4">
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator"
+                                src="https://crehana-blog.s3.amazonaws.com/media/filer_public/af/f3/aff302fb-8cbc-4dfc-b648-bc467ed3fb14/como-iniciarte-en-diseno-lunes.jpg">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">Pautas
+                                <i class="material-icons right">more_vert</i></span>
+                            <div class="card-action center">
+
+                                <button class="btn black waves-effect waves-teal">Saber MAs</button>
+
+                            </div>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Pautas<i
+                                    class="material-icons right">close</i></span>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum at tenetur mollitia
+                                voluptate? Tempore, itaque.</p>
+                            <img src="https://crehana-blog.s3.amazonaws.com/media/filer_public/af/f3/aff302fb-8cbc-4dfc-b648-bc467ed3fb14/como-iniciarte-en-diseno-lunes.jpg"
+                                class="responsive-img">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+    </main>
+
+    <footer class="page-footer black">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col s12 m6 l6">
+                    <h5 class="white-text">Contactos</h5>
+                    <p class="white-text">3228592924</p>
+                </div>
+
+                <div class="col s12 m6 l6">
+
+                    <h5 class="white-text">Enlaces De Interes</h5>
+
+                    <ul>
+                        <li><a href="https://www.dian.gov.co/" target="_blank" class="white-text" href="#!">Dian</a>
+                        </li>
+                        <li><a class="white-text" href="#!">Link 2</a></li>
+                        <li><a class="white-text" href="#!">Link 3</a></li>
+                        <li><a class="white-text" href="#!">Link 4</a></li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="social-container">
+
+            <a href="https://www.facebook.com/contapaulamarcela" target="_blank"><i
+                    class="fab fa-facebook-square"></i></a>
+            <a href="https://www.instagram.com/contapaulamarcela" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.twitter.com/contapaulamarce" target="_blank"><i class="fab fa-twitter"></i></a>
+
+        </div>
+
+        <div class="footer-copyright">
+            <div class="container">
+                Hecho Por Paula Farias Con Materialize</a>
+            </div>
+        </div>
+    </footer>
+
+    <!-- BOTON DE ACCTION -->
+
+    <div class="fixed-action-btn">
+        <a class="btn-floating btn-large black">
+            <i class="large material-icons">mode_edit</i>
+        </a>
+        <ul>
+            <li><a href="https://api.whatsapp.com/send?phone=573142869919&text=Hola%20Me%20Interesa%20Saber%20Sobre%20:"
+                    target="_blank" class="btn-floating red tooltipped" data-position="left" data-tooltip="Whatsapp"><i
+                        class="material-icons">insert_chart</i></a></li>
+            <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+            <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+            <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+        </ul>
+    </div>
+
+    <!-- Para Mobil -->
+
+    <ul class="sidenav" id="mobile-demo">
+        <li href="#!" class="brand-logo center">Logo</li>
+        <li><a href="#">Noticias</a></li>
+        <li><a href="#">Noticias Paula</a></li>
+        <li><a href="#">Mas De Paula</a></li>
+    </ul>
+
+    <script src="https://kit.fontawesome.com/35db202371.js" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.6.1.slim.min.js"
+        integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA=" crossorigin="anonymous"></script>
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+
+    <script>
+    $(document).ready(function() {
+        $('.sidenav').sidenav();
+        $('.slider').slider();
+        $('.fixed-action-btn').floatingActionButton();
+        $('.tooltipped').tooltip();
+    });
+    </script>
+
+</body>
+
+</html>
